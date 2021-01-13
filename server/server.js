a0a34159b3e519f9e8f62a15caf5f15bb3d68bcc
catse2000 <catse2000@yahoo.com>
2021-01-12 18:53:21 -0600

    test

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ db.once('open', () => {
 });
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // app.use(routes);
 
