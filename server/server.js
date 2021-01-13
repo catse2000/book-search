4ec44821a02f601fac7042c7a75f7a75acde63fe
catse2000 <catse2000@yahoo.com>
2021-01-12 18:04:24 -0600

    updated app.listen

--- server/server.js
+++ server/server.js
@@ -26,8 +26,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
 
 // if we're in production, serve client/build as static assets
